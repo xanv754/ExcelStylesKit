c8bd091a77da571b43621ec668b8f82aaaf578d2
xanv754 <angyeemarin.dev@gmail.com>
2026-03-26 00:20:24 -0400

    feat: add new variable

--- excelstyleskit/constants/__init__.py
+++ excelstyleskit/constants/__init__.py
@@ -1,9 +1,10 @@
 from excelstyleskit.constants.alphabet import STRING_ALPHABET, NUMBER_ALPHABET
-from excelstyleskit.constants.variables import COLUMN_MAX
+from excelstyleskit.constants.variables import COLUMN_MAX, ROW_MAX
 
 
 __all__ = [
     STRING_ALPHABET,
     NUMBER_ALPHABET,
-    COLUMN_MAX
+    COLUMN_MAX,
+    ROW_MAX
 ]
